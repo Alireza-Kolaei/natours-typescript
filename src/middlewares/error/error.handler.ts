596a0642fca162004f0f325ac2859289045893df
Alireza Kolaei <kolaei.alireza79@gmail.com>
2023-07-13 22:34:53 +0330

    ./error.transittion

--- src/middlewares/error/error.handler.ts
+++ src/middlewares/error/error.handler.ts
@@ -1,7 +1,7 @@
 import { Application } from 'express';
 import * as httpStatus from 'http-status';
 import ApiError from '../../utils/api-error.helper';
-import { errorHandler } from './error.transittion';
+import { errorHandler } from './error.transition';
 export default (app: Application) => {
   app.use((req, res, next) => {
     next(new ApiError(httpStatus.NOT_FOUND, 'Requested Resource Was Not Found'));
